90a13350233eff38f2a3b444ba364c315ed2f7eb
xxjokerx <common.hori@protonmail.com>
2022-08-05 14:03:30 +0200

    now the app can output running containers details to file, then its parsed correctly. Next step : add logic to spot testcontainers images and delete it

--- src/main/java/org/devnotfound/testcontainerscrontask/CLI.java
+++ src/main/java/org/devnotfound/testcontainerscrontask/CLI.java
@@ -12,31 +12,32 @@ import java.util.List;
 public class CLI {
     private static final Logger logger = LoggerFactory.getLogger(CronTask.class);
 
-    static List<String> getContainersId() throws IOException {
+    static List<String> getContainersId() throws IOException, InterruptedException {
         ProcessBuilder pb = new ProcessBuilder("docker", "container", "ls", "--quiet");
         File containers = new File("src/main/resources/containers-id");
         pb.redirectErrorStream(true);
         pb.redirectOutput(ProcessBuilder.Redirect.to(containers));
 
+        Process p = pb.start();
+        p.waitFor();
+
         String s = Files.readString(Path.of("src/main/resources/containers-id"));
-        pb.start();
-
-
 
         return List.of(s.trim().split("\\n"));
 
     }
 
-    static void inspect(String id) throws IOException {
+    static void inspect(String id) throws IOException, InterruptedException {
         logger.info(id);
 
         ProcessBuilder pb = new ProcessBuilder("docker", "inspect", "--format={{json .}}" ,  id);
 
         String filepath = "src/main/resources/container-" + id + ".json";
-        File details = new File(filepath);
+        File file = new File(filepath);
         pb.redirectErrorStream(true);
-        pb.redirectOutput(ProcessBuilder.Redirect.to(details));
+        pb.redirectOutput(ProcessBuilder.Redirect.to(file));
 
-        pb.start();
+        Process p = pb.start();
+        p.waitFor();
     }
 }
